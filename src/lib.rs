e7608a5c5533eec274a017a213e9822956cd271c
chupson <chupson@chupson.dev>
2024-02-12 10:36:47 +0100

    move algorithm module under spellchecker module

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,6 @@ pub mod render; // responsible for rendering the ratatui tui
 pub mod update; // updates every frame, handles input
 
 // the actual logic behind spellchecking
-pub mod algorithm;
 pub mod spellchecker; // the main module controlling spellchecking // the algorithms for calculating word distance and similar
 
 pub mod prelude; // global exports and other
