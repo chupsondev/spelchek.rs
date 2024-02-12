5e150fb62e400de1685b9a40c56b6cb78a3f460f
chupson <chupson@chupson.dev>
2024-02-12 09:56:54 +0100

    create basic project structure

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// Define modules
+pub mod app_state; // holds the app state
+pub mod render; // responsible for rendering the ratatui tui
+pub mod update; // updates every frame, handles input
+
+// the actual logic behind spellchecking
+pub mod algorithm;
+pub mod spellchecker; // the main module controlling spellchecking // the algorithms for calculating word distance and similar
+
+pub mod prelude; // global exports and other
+use crate::prelude::*;
---
+++ src/main.rs
@@ -0,0 +1,5 @@
+use spelchek::prelude::*;
+
+fn main() -> Result<()> {
+    Ok(())
+}
