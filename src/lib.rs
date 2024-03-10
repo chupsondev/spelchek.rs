6d0d2b1b704dfeb135a448a5e42b19ed4d1509c6
chupson <chupson@chupson.dev>
2024-03-10 11:54:17 +0100

    Remove stray, wrong comment
    In `lib.rs`, for some reason, there was a comment that was just straight
    up factually incorrect and very much so not called for. This issue was,
    I believe, introduced in e7608a5c5533eec274a017a213e9822956cd271c.

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub mod render; // responsible for rendering the ratatui tui
 pub mod update; // updates every frame, handles input
 
 // the actual logic behind spellchecking
-pub mod spellchecker; // the main module controlling spellchecking // the algorithms for calculating word distance and similar
+pub mod spellchecker; // the main module controlling spellchecking
 
 pub mod prelude; // global exports and other
 use crate::prelude::*;
